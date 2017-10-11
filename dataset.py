380d44d640b2ee6955fe43f50842ba17393744a9
Sean Shahkarami <sean.shahkarami@gmail.com>
2017-10-11 13:10:45 -0500

    updated dataset load to just use built-in pandas csv reader

--- dataset.py
+++ dataset.py
@@ -1,56 +1,24 @@
 import pandas as pd
-import requests
-import gzip
-import datetime
-import re
 
 
-def guess_type(x):
-    # numeric type
-    if re.match('^[+-]?[0-9\.]+$', x):
-        return float(x)
+def load_dataset(url):
+    column_names = ['node_id', 'timestamp', 'plugin', 'topic', 'sensor', 'param', 'value']
+    df = pd.read_csv(url, sep=';', names=column_names, parse_dates=['timestamp'])
 
-    # default leaves alone
-    return x
+    series = []
 
+    for (sensor, param), rows in df.groupby(('sensor', 'param')):
+        name = '.'.join([sensor, param])
 
-def load(blob):
-    events = {}
+        try:
+            s = pd.Series(rows['value'].astype(float).values, index=rows['timestamp'].values, name=name)
+        except ValueError:
+            continue
 
-    lines = gzip.decompress(blob).decode().splitlines()
+        series.append(s)
 
-    for line in lines:
-        fields = line.split(';')
-
-        timestamp = datetime.datetime.strptime(fields[1], '%Y/%m/%d %H:%M:%S')
-        # plugin = fields[3]
-        sensor = fields[4]
-        param = fields[5]
-        value = guess_type(fields[6])
-
-        if timestamp not in events:
-            events[timestamp] = {}
-
-        events[timestamp]['.'.join([sensor, param])] = value
-
-    rows = list(pd.DataFrame(events).items())
-
-    return pd.DataFrame([r[1] for r in rows], index=[r[0] for r in rows])
+    return pd.concat(series, axis=1)
 
 
-def load_from_file(filename):
-    """Load a dataset from a file."""
-    with open(filename, 'rb') as f:
-        return load(f.read())
-
-
-def load_from_url(url):
-    """Load a dataset from a URL."""
-    r = requests.get(url)
-    assert r.status_code == 200
-    return load(r.content)
-
-
-def load_from_urls(urls):
-    """Loads and concatenates multiple datasets from a list of URLs."""
-    return pd.concat([load_from_url(url) for url in urls])
+def load_datasets(urls):
+    return pd.concat([load_dataset(url) for url in urls])
